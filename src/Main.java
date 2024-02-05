4d4902c34c9dba26087eedd2e4650dfc9d7f2109
AllaVyz <efialka@mail.ru>
2024-02-05 18:13:00 +0300

    change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketprice = 550; // стоимость билета
+        int ticketPrice = 550; // стоимость билета
 
-        int bonusMiles = ticketprice/20; // стоимость бонусных миль
+        int bonusMiles = ticketPrice / 20; // стоимость бонусных миль
 
         System.out.println("Количество миль за купленный билет");
         System.out.println(bonusMiles);
