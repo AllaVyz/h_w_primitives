e1d9a7bc2c8742a2c75922c51540ac3b038dbdae
AllaVyz <efialka@mail.ru>
2024-02-05 13:07:38 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketprice = 550; // стоимость билета
+
+        int bonusMiles = ticketprice/20; // стоимость бонусных миль
+
+        System.out.println("Количество миль за купленный билет");
+        System.out.println(bonusMiles);
+    }
+}
